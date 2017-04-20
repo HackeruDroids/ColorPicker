4bd1e877463dedb9c90594a778e739a8ba0fe995
hackerudroids <tomerjava@gmail.com>
2017-04-20 21:25:04 +0300

    colorPicker

--- app/src/main/java/tomer/edu/colorpicker/MainActivity.java
+++ app/src/main/java/tomer/edu/colorpicker/MainActivity.java
@@ -1,5 +1,6 @@
 package tomer.edu.colorpicker;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -7,8 +8,9 @@ import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
-    EditText etRed;
-    SeekBar sbRed;
+    EditText etRed, etGreen, etBlue;
+    EditText etResult;
+    SeekBar sbRed, sbGreen, sbBlue;
 
 
     @Override
@@ -17,14 +19,48 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         setContentView(R.layout.activity_main);
 
         etRed = (EditText) findViewById(R.id.etRed);
+        etBlue = (EditText) findViewById(R.id.etBlue);
+        etGreen = (EditText) findViewById(R.id.etGreen);
+
+        etResult = (EditText) findViewById(R.id.etResult);
+
+
         sbRed = (SeekBar) findViewById(R.id.sbRed);
+        sbGreen = (SeekBar) findViewById(R.id.sbGreen);
+        sbBlue = (SeekBar) findViewById(R.id.sbBlue);
 
         sbRed.setOnSeekBarChangeListener(this);
+        sbGreen.setOnSeekBarChangeListener(this);
+        sbBlue.setOnSeekBarChangeListener(this);
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        etRed.setText(String.valueOf(progress));
+
+        if (seekBar == sbRed){
+            etRed.setText(String.valueOf(progress));
+        }
+        else if (seekBar == sbGreen){
+            etGreen.setText(String.valueOf(progress));
+        }
+        else if (seekBar == sbBlue){
+            etBlue.setText(String.valueOf(progress));
+        }
+
+
+        int red = sbRed.getProgress();
+        int green = sbGreen.getProgress();
+        int blue = sbBlue.getProgress();
+
+        int rgb = Color.rgb(red, green, blue);
+        etResult.setBackgroundColor(rgb);
+
+        String hexString = Integer.toHexString(rgb);
+        etResult.setText(hexString);
+
+        /*etRed.setText(String.valueOf(sbRed.getProgress()));
+        etGreen.setText(String.valueOf(sbGreen.getProgress()));
+        etBlue.setText(String.valueOf(sbBlue.getProgress()));*/
     }
 
     @Override
